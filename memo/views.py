d56aaaa8fc99d9cb6f1be0c7e383abcf1c9282a2
errorfixie <65066542+errorfixie@users.noreply.github.com>
2020-10-19 11:08:54 +0900

    생성버튼구현

--- memo/views.py
+++ memo/views.py
@@ -20,30 +20,32 @@ def MemoHomeView(request):
     
     if request.method == "POST": # POST로 들어오면
         form = MemoCreateForm(request.POST) # 들어온 값들을 MemoCreateForm에 넣어 객체생성
-        form.save() # 저장
 
-        # 폼에 넣은 값 변수로 저장
-        title = request.POST.get('title')
-        contents = request.POST.get('contents')
-        
-        if Memo.objects.filter(title=title, contents=contents).order_by('-memodate'):
-            memo = Memo.objects.filter(title=title, contents=contents).order_by('-memodate')[0]
-        else:
-            memo = Memo.objects.filter(title=title, contents=contents).order_by('-memodate') # title과 contents가 같지만 가장 최신 데이터 객체
-        username = request.user
+        if form.is_valid():
+            form.save() # 저장
 
-        # print(memo.memodate) # 변수확인
-        # print(memo.title)
-        # print(memo.contents)
-        # print(memo.pk)
-        
-        # usermemo테이블에 userNUM, memoNUM 저장하기
-        usermemo = Usermemo() # Usermemo객체 생성
-        usermemo.userNUM = username 
-        usermemo.memoNUM_id = memo.pk
-        usermemo.save() 
+            # 폼에 넣은 값 변수로 저장
+            title = request.POST.get('title')
+            contents = request.POST.get('contents')
+            
+            if Memo.objects.filter(title=title, contents=contents).order_by('-memodate'):
+                memo = Memo.objects.filter(title=title, contents=contents).order_by('-memodate')[0]
+            else:
+                memo = Memo.objects.filter(title=title, contents=contents).order_by('-memodate') # title과 contents가 같지만 가장 최신 데이터 객체
+            username = request.user
 
-        return HttpResponseRedirect('/memo/') # 저장후 리다이렉트할 url 지정
+            # print(memo.memodate) # 변수확인
+            # print(memo.title)
+            # print(memo.contents)
+            # print(memo.pk)
+            
+            # usermemo테이블에 userNUM, memoNUM 저장하기
+            usermemo = Usermemo() # Usermemo객체 생성
+            usermemo.userNUM = username 
+            usermemo.memoNUM_id = memo.pk
+            usermemo.save() 
+
+            return HttpResponseRedirect('/memo/') # 저장후 리다이렉트할 url 지정
 
     else:
         form = MemoCreateForm() # POST가 아니면 그냥 폼만 보여줌
